e8e73da2d6c0d589699a8bb6a83839867d17386a
DrHouseUA <42034787+DrHouseUA@users.noreply.github.com>
2022-11-23 21:31:36 +0200

    Add files via upload
    Practice in coding. Sorting byte array by using Map and Map.Entry methods.

--- Main.java
+++ Main.java
@@ -2,9 +2,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+
+
         Path path = Paths.get("D:\\Programm learning\\Neeeded Skills\\schedule4.pdf");
         File file = new File(path.toUri());
         FileInputStream fileInputStream = null;
@@ -17,22 +20,21 @@ public class Main {
             fileInputStream.close();
 
             int arrayLength = 256;
-            int[] arrayOfBytes = new int[arrayLength];
-
+            Integer[] arrayOfBytes = new Integer[arrayLength];
+            Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+            
             for (int i = 0; i < arrayLength; i++) {
-                    arrayOfBytes[i]=0;
+                arrayOfBytes[i]=0;
             }
 
             for (int i = 0; i < bFile.length; i++) {
-                   arrayOfBytes[bFile[i]+128]+=1;
+                arrayOfBytes[bFile[i]+128]+=1;
             }
-
-            System.out.println("Counts of byte:");
-            for (int i = 0; i < arrayLength; i++) {
-                if(arrayOfBytes[i] !=0) {
-                    System.out.println("Bytes " + i + " seems " + arrayOfBytes[i] + " times.");
-                }
+            for (int i = 0; i < arrayOfBytes.length; i++) {
+                map.put(i, arrayOfBytes[i]);
             }
+            System.out.println("Byte=Counts");
+            map.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).forEach(System.out::println);
         }
         catch (Exception e)
         {
